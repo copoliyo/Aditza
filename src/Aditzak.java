6646ac6d3e9e6eaac8d12fd96d5b3b987f3cb635
copoliyo portatil <copoliyo@gmail.com>
2013-12-16 17:48:38 +0100

    Commit desde Eclipse

---
+++ src/Aditzak.java
@@ -0,0 +1,63 @@
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+public class Aditzak  extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -491034458093394202L;
+
+	public Aditzak(){
+		System.out.println("Segunda modificaciï¿½n con Git");
+		System.out.println("Segunda modificaciï¿½n con Git");
+		System.out.println("AÃ±adido con copoliy@gmail.com desde Bonito");
+		System.out.println("Añadido desde Eclipse con EGit");
+		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//creamos el contenedor
+		setTitle("Aditzak");
+		setSize(350, 200);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setFont( new Font( "Courier",Font.PLAIN,30 ));
+		
+		System.out.println("Primera modificaciï¿½n con Git");
+
+		//Aï¿½adimos los componentes de la pantalla al contenedor		
+		PantallaAditzak pa = new PantallaAditzak();
+		this.add(pa);			
+		setVisible(true);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Aditzak();
+		
+		System.out.println("Tercera modificaciï¿½n con Git");
+	}
+
+}
